e138873b90882e8fe94d10e7547fb31978a8a6a1
Dina Taldytova <drtaldytova@gmail.com>
2023-06-11 13:10:58 +0300

    demo lite-bank creating

---
+++ lite-bank/account/src/main/scala/misis/model/Account.scala
@@ -0,0 +1,21 @@
+package misis.model
+
+import java.time.Instant
+import java.util.UUID
+
+case class Account(id: Int, amount: Int) {
+    def update(value: Int) = this.copy(amount = amount + value)
+}
+
+trait Command
+case class AccountUpdate(accountId: Int, value: Int, category: Option[String], tags: Option[Seq[String]])
+
+trait Event
+case class AccountUpdated(
+    operationId: UUID = UUID.randomUUID(),
+    accountId: Int,
+    value: Int,
+    publishedAt: Option[Instant] = Some(Instant.now()),
+    category: Option[String],
+    tags: Option[Seq[String]]
+)
